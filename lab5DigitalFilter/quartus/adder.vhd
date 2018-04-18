0bfeeb3985bab6aa5a80a31c06a870d6550b5789
sickle5 <revanpotter@verizon.net>
2018-04-17 21:16:45 -0400

    Finished adding everything.  Currently does not compile

--- lab5DigitalFilter/quartus/adder.vhd
+++ lab5DigitalFilter/quartus/adder.vhd
@@ -1,4 +1,4 @@
---based off of https://en.wikibooks.org/wiki/VHDL_for_FPGA_Design/4-Bit_Adder changed from 4 to 32 bit
+--based off of https://en.wikibooks.org/wiki/VHDL_for_FPGA_Design/4-Bit_adder changed from 4 to 32 bit
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -7,16 +7,16 @@ use ieee.std_logic_signed.all;
 use ieee.std_logic_textio.all;
 use std.textio.all;
 
-entity Adder is
+entity adder is
    port
    (
       nibble1, nibble2 : in unsigned(35 downto 0); 
       sum       : out unsigned(35 downto 0); 
-      carry_out : out std_logic
+      --carry_out : out std_logic
    );
-end entity Adder;
+end entity adder;
  
-architecture Behavioral of Adder is
+architecture Behavioral of adder is
    signal temp : unsigned(36 downto 0); 
 begin 
    temp <= ("0" & nibble1) + nibble2; 
@@ -24,5 +24,5 @@ begin
    -- temp <= ('0' & nibble1) + ('0' & nibble2);
 
    sum       <= temp(35 downto 0); 
-   carry_out <= temp(36);
+   --carry_out <= temp(36);
 end architecture Behavioral;
--- lab5DigitalFilter/quartus/audiofilter.vhd
+++ lab5DigitalFilter/quartus/audiofilter.vhd
@@ -13,23 +13,52 @@ signal filterOutput : signed(35 downto 0);
 
 signal s1Output : signed(35 downto 0);
 signal b1input : signed(35 downto 0);
+signal b12input : signed(35 downto 0);
 signal b1Output : signed(35 downto 0);
 signal ab2Output : signed(35 downto 0);
+
+signal ff21output : signed(35 downto 0);
+signal ff22output : signed(35 downto 0);
+
+signal ff31output : signed(35 downto 0);
+signal ff32output : signed(35 downto 0);
+
+signal s2adder1Output : signed(35 downto 0);
+signal s2adder2Output : signed(35 downto 0);
+signal s2adder3Output : signed(35 downto 0);
+
+signal badder2output : signed(35 downto 0);
 signal b2Output : signed(35 downto 0);
+signal b12Output : signed(35 downto 0);
+signal b13Output : signed(35 downto 0);
+signal b22Output : signed(35 downto 0);
+signal b23Output : signed(35 downto 0);
+signal b32Output : signed(35 downto 0);
+signal b33Output : signed(35 downto 0);
+
 signal a2Output : signed(35 downto 0);
-signal sec1 : signed(35 downto 0);
-signal sec2 : signed(35 downto 0);
+signal a22Output : signed(35 downto 0);
+signal a23Output : signed(35 downto 0);
+signal a32Output : signed(35 downto 0);
+signal a33Output : signed(35 downto 0);
+
+signal sec1Output : signed(35 downto 0);
+signal sec1       : signed(35 downto 0);
+signal sec2       : signed(35 downto 0);
 
 --constant declarations
-constant B(1)(1) : std_logic_signed := x"E45";
-constant B(3)(1) : std_logic_signed := (others => '0');
-constant B(1)(2) : std_logic_signed := x"15B"; --.0026446 to 347
-constant B(2)(2) : std_logic_signed := x"2B7"; -- .00529893 to 695
-constant B(3)(2) : std_logic_signed := x"15B"; --.0026446
-constant a(2)(1) : std_logic_signed := x"1D1EC";-- - 91
-constant a(3)(1) : std_logic_signed := (others => '0');
-constant a(2)(2) : std_logic_signed := x"1D1EC"-- -1.9349
-constant a(3)(2) : std_logic_signed := x"1E317"-- .94353 to 123671
+constant B(1)(1) : std_logic_signed(35 downto 0) := x"E45";
+constant B(3)(1) : std_logic_signed(35 downto 0) := (others => '0');
+constant B(1)(2) : std_logic_signed(35 downto 0) := x"15B"; --.0026446 to 347
+constant B(2)(2) : std_logic_signed(35 downto 0) := x"2B7"; -- .00529893 to 695
+constant B(3)(2) : std_logic_signed(35 downto 0) := x"800B"; --.0026446
+constant B(3)(3) : std_logic_signed(35 downto 0) := x"15B"; --.25008 to 327793
+
+constant a(2)(1) : std_logic_signed(35 downto 0) := x"1D1EC";-- - 91
+constant a(3)(1) : std_logic_signed(35 downto 0) := (others => '0');
+constant a(2)(2) : std_logic_signed(35 downto 0) := x"1D1EC"-- -1.9349
+constant a(3)(2) : std_logic_signed(35 downto 0) := x"1E317"-- .94353 to 123671
+constant a(3)(3) : std_logic_signed(35 downto 0) := x"1B79C"-- .85861 TO 1122540
 
 -- Grab just one channel from input
 filterInOneChannel <= i_audioSample(35 downto 0);
@@ -70,12 +99,11 @@ architecture arch of audiofilter is
 		);
 	end component;
 
-	component Adder is
+	component adder is
 		port
 		(
 			nibble1, nibble2 : in unsigned(35 downto 0); 
 			sum       : out unsigned(35 downto 0); 
-			carry_out : out std_logic
 		);
 	end component;
 	
@@ -92,7 +120,10 @@ architecture arch of audiofilter is
 		-- result => s1Output);
 		--done with filterSection_1in
 		
-	s(1)adder: Adder port map(
+        
+    --sec1
+    
+	s(1)adder: adder port map(
 		nibble1 => filterSection_1in,
 		nibble1 => a2Output,
 		sum => b1input,
@@ -105,7 +136,7 @@ architecture arch of audiofilter is
 		result => b1Output
 	)
 	
-	badder: Adder port map(
+	badder: adder port map(
 		nibble1 => b1Output,
 		nibble2 => b2Output,
 		sum => sec1
@@ -119,15 +150,157 @@ architecture arch of audiofilter is
 	)
 	b(2)(1)multi: multi_inst port map(
 		dataa => ab2Output,
-		datab => b2Output,
+		datab => B(2)(1),
 		result => b2Output
 	)
 	a(2)(1)multi: multi_inst port map(
 		dataa => ab2Output,
-		datab => b2Output,
+		datab => a(2)(1),
 		result => a2Output
 	)
-	
+    
+    --sec1.5
+    
+    s1adder: adder port map(
+        nibble1 => sec1,
+        nibble2 => a22Output,
+        sum => sec1Output
+    )
+    
+    s12adder: adder port map(
+        nibble1 => sec1Output,
+        nibble2 => a22Output,
+        sum     => b12input
+    )
+    
+    b12multi: multi_inst port map(
+    	dataa => b12input,
+		datab => b(2)(1),
+		result => b12Output
+    )
+    
+    badder2: adder port map(
+        nibble1 => b12Output,
+        nibble2 => b22Output,
+        sum     => badder2output,
+    )
+    
+    badder3: adder port map(
+        nibble1 => badder2output,
+        nibble2 => b32Output,
+        sum     => sec2,
+    )
+    
+    b22multi: multi_inst port map(
+    	dataa => ff21output,
+		datab => b(2)(2),
+		result => b22Output
+    )
+    
+    a22multi: multi_inst port map(
+    	dataa => ff21output,
+		datab => a(2)(2),
+		result => a22Output
+    )
+    
+    a32multi: multi_inst port map(
+    	dataa => ff22output,
+		datab => a(3)(2),
+		result => a32Output
+    )
+    
+    b32multi: multi_inst port map(
+    	dataa => ff22output,
+		datab => b(3)(2),
+		result => b22Output
+    )
+    
+    s1ff1 :flipflop port map(
+        d => b12input,
+        clk => clk,
+        en => dataReq,
+        q => ff21output
+    )
+
+    s1ff2 :flipflop port map(
+        d => ff21output,
+        clk => clk,
+        en => dataReq,
+        q => ff22output
+    )
+    
+    --section 2
+    
+    s2adder1: adder port map(
+        nibble1 => sec2,
+        nibble2 => a23Output,
+        sum     => s2adder1Output
+    )
+    
+    s2adder2: adder port map(
+        nibble1 => s2adder1Output,
+        nibble2 => a33Output,
+        sum     => s2adder2Output
+    )
+    
+    b13multi: multi_inst port map(
+        dataa => s2adder2Output,
+        datab => B(1)(3),
+        result => b13Output
+    )
+    
+    s2adder3:adder port map(
+        nibble1 => b13Output,
+        nibble2 => b23Output,
+        sum     => s2adder3Output
+    )
+    
+    s2ff1: flipflop port map(
+        d => s2adder2Output,
+        clk => clk,
+        en => dataReq,
+        q=> ff31output
+    )
+    
+    s2ff2: flipflop port map(
+        d => ff31output,
+        clk => clk,
+        en => dataReq,
+        q=> ff32output
+    )
+    
+    a23multi: multi_inst port map(
+        dataa => ff31output,
+        datab => a(2)(3),
+        result => a23Output
+    )
+    
+    b23multi: multi_inst port map(
+        dataa => ff31output,
+        datab => B(2)(3),
+        result => b23Output
+    )
+    
+    a33multi: multi_inst port map(
+        dataa => ff32output,
+        datab => a(3)(3),
+        result => a33Output
+    )
+    
+    b33multi: multi_inst port map(
+        dataa => ff32output,
+        datab => B(3)(3),
+        result => b33Output
+    )
+    
+    s2adder4:adder port map(
+        nibble1 => s2adder3Output,
+        nibble2 => b33Output,
+        sum     => audioSampleFiltered
+    )
+    
+    
+    
 	
 	if (rising_edge(clk)) then
 		if (reset = ‘1’) then
